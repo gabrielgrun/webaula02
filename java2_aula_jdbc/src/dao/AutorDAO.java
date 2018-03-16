a168063d7b3ac2ca4311ba6c7aa6df40371da72d
gg005249 <gg005249@L0106.LABORATORIO>
2018-03-15 21:54:28 -0300

    Fim da aula 15/03 - Feitos util, model, forms, dao

---
+++ java2_aula_jdbc/src/dao/AutorDAO.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Autor;
+import util.ConnectionJDBC;
+
+public class AutorDAO {
+    
+    Connection connection;
+    
+    public AutorDAO() throws Exception {
+       //Obtém uma conexão 
+       connection = ConnectionJDBC.getConnection();
+    }
+    
+    public void save(Autor autor) throws Exception {
+        String SQL = "INSERT INTO AUTOR(NOME) VALUES(?)";
+        try {
+            PreparedStatement p = connection.prepareStatement(SQL);
+            //p.setInt(1, autor.getAutor_id());
+            p.setString(1, autor.getNome());
+            p.execute();
+        } catch (SQLException ex) {
+            throw new Exception(ex);
+        }
+    }
+    
+    public void update(Autor autor){
+        
+    }
+    
+    public void delete(Autor autor){
+        
+    }
+    
+    public Autor findById(int id){
+        return new Autor();
+    }
+    
+    public List<Autor> findAll() {
+        return new ArrayList<>();
+    }
+}
--- java2_aula_jdbc/src/util/ConnectionJDBC.java
+++ java2_aula_jdbc/src/util/ConnectionJDBC.java
@@ -1,7 +1,49 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ConnectionJDBC {
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws Exception {
+
+        if (connection == null) {
+
+            try {
+                //Testar o carregamento da classe (Driver)
+                Class.forName("org.firebirdsql.jdbc.FBDriver");
+            } catch (ClassNotFoundException ex) {
+                throw new Exception(ex);
+            }
+
+            //Dados de conexão
+            String servidor = "192.168.56.101";
+            String database = "/databases/aula07.fdb";
+            String user = "SYSDBA";
+            String password = "masterkey";
+
+            //Prepara a URL de conexão
+            //jdbc:firebirdsql:IP/3050:/diretorio/do/banco.fdb?parametros adicionais
+            String url = "jdbc:firebirdsql:" + servidor + "/3050:" + database + "?encoding=WIN1252";
+
+            //Obtém a conexão com o banco de dados
+            connection = DriverManager.getConnection(url, user, password);
+
+        }
+
+        return connection;
+    }
+
     public static void main(String[] args) {
         System.out.println("Teste!");
+        try {
+            ConnectionJDBC.getConnection();
+        } catch (Exception ex) {
+            System.out.println(""+ex.getMessage());
+        }
     }
 }
